9ec57e24b40693aad7a49e7dc16ba7d4fca78077
JackChanCJ <JackChanCJ@users.noreply.github.com>
2017-05-27 13:57:50 +0800

    company

--- pages/basePage.py
+++ pages/basePage.py
@@ -31,24 +31,28 @@ class Page(object):
         self.driver.title
 
     def enter_page(self, mkmc, ymmc):
+        """
+
+        :rtype: object
+        """
         self.driver.switch_to.default_content()
         self.driver.switch_to.frame("menuFrame")
         # 方法1
-        # mk_loc = u"//span[text()='\"+ mkmc +\"']"
-        # ym_loc = u"//span[@sjmc='\"+ mkmc +\"' and text()='\"+ ymmc +\"']"
-        # self.driver.find_element_by_xpath(mk_loc).click()
-        # self.driver.find_element_by_xpath(ym_loc).click()
+        mk_loc = u"//span[text()='\"+ mkmc +\"']"
+        ym_loc = u"//span[@sjmc='\"+ mkmc +\"' and text()='\"+ ymmc +\"']"
+        self.driver.find_element_by_xpath(mk_loc).click()
+        self.driver.find_element_by_xpath(ym_loc).click()
         # 方法2
-        mks = u"//span[@class='folder']"
-        mk_elements = self.driver.find_elements_by_xpath(mks)
-        for mk_element in mk_elements:
-            if mk_element.text == mkmc:
-                mk_element.click()
-                yms = u"//li[@sjmc='\" + mkmc +\" ']"
-                ym_elements = self.driver.find_elements_by_xpath(yms)
-                for ym_element in ym_elements:
-                    if ym_element.text == ymmc:
-                        ym_element.click()
+        # mks = u"html/body/ul/li[@onmouseout='topMosOut(this)' and @onmouseover='topMosOver(this)']/span"
+        # mk_elements = self.driver.find_elements_by_xpath(mks)
+        # for mk_element in mk_elements:
+        #     if mk_element.text == mkmc:
+        #         mk_element.click()
+        #         yms = u"//li[@sjmc='\" + mkmc +\" ']"
+        #         ym_elements = self.driver.find_elements_by_xpath(yms)
+        #         for ym_element in ym_elements:
+        #             if ym_element.text == ymmc:
+        #                 ym_element.click()
 
     def random_select_box(self, loc):
         # c_ele下拉框元素
